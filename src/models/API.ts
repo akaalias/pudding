42422c971132d282b555c236cc2f8782bb5ecab9
Alexis Rondeau <alexis.rondeau@gmail.com>
2022-08-21 19:57:22 +0200

    Off screen

--- src/models/API.ts
+++ src/models/API.ts
@@ -2,6 +2,7 @@ export default class API {
     async get(query: string) {
         return []
     }
+
     public static async getTransactions(address: string) {
         const requestURL = 'https://chain.api.btc.com/v3/address/15urYnyeJe3gwbGJ74wcX89Tz7ZtsFDVew/tx'
 
