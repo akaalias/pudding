f58fc3d489f34cacb3ea58f1a21d82e8ad066ecd
Alexis Rondeau <alexis.rondeau@gmail.com>
2022-09-26 15:38:55 +0200

    Changes depth

--- src/models/Constants.ts
+++ src/models/Constants.ts
@@ -1,15 +1,16 @@
 export default class Constants {
-    public static maxDepth = 2
+    public static maxDepth = 3
 
     public static offsetForDepth(depth: number) {
+
         if(depth >= 3) {
-            return 50
+            return 80
         }
         if(depth == 2) {
-            return 25
+            return 40
         }
         if(depth == 1) {
-            return 12
+            return 10
         }
 
         return 1
