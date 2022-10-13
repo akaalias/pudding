9832575f41d9a120fdb111d3222a495a2ea7ad33
Alexis Rondeau <alexis.rondeau@gmail.com>
2022-10-13 14:08:31 +0200

    adds production flag

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+module.exports = defineConfig({publicPath: process.env.NODE_ENV === 'production'
+      ? '/pudding/'
+      : '/',
   outputDir: 'docs',
   transpileDependencies: [
     'vuetify'
