5895df749e5c9f5c75dfa205dbb353419554e0e7
Alexis Rondeau <alexis.rondeau@gmail.com>
2022-10-26 14:09:21 +0200

    Adds backdrop

--- src/views/LandingpageView.vue
+++ src/views/LandingpageView.vue
@@ -11,7 +11,7 @@
           <h1 class="display-3"><span class="highlight">Follow the money, connect the dots, find the story.</span></h1>
           <br>
           <h2 class="display-2"><span class="highlight-2">
-            <i>Pudding</i> the FUN back into forensic analysis and reconnaissance of crypto-currency token ecosystems</span>
+            Putting the FUN back into forensic analysis and reconnaissance of crypto-currency token ecosystems</span>
           </h2>
         </v-col>
       </v-row>
@@ -85,11 +85,10 @@
         </v-col>
       </v-row>
     </v-container>
-
-    <v-container id="explore-addresses-container">
+    <v-container fluid id="explore-addresses-container">
       <v-row>
         <v-col cols="md-12 centered">
-          <h2>Explore the Transaction Networks of Individual Ethereum Addresses</h2>
+          <h2><span class="highlight">NEW! Explore the Transaction Networks of Individual Ethereum Addresses</span></h2>
           <v-btn
               elevation="1"
               light
@@ -108,7 +107,7 @@
       </v-row>
     </v-container>
 
-    <v-container fluid id="contact-container" class="contact">
+    <v-container fluid light id="contact-container" class="contact">
       <v-row>
         <v-col cols="md-2">
         </v-col>
@@ -184,14 +183,17 @@ export default Vue.extend({
 h1.display-3 {
   margin-top: 10%;
   line-height: 120%;
+  transform: rotate(-5deg);
 }
 
 h2.display-2 {
   font-size: 36px !important;
   line-height: 120%;
+  transform: rotate(-5deg);
 }
 
 .highlight {
+  padding: 5px;
   background-color: blueviolet;
 }
 
@@ -218,6 +220,11 @@ h2.display-2 {
   margin-bottom: 30px;
 }
 
+#contact-container {
+  padding-top: 50px;
+  background-color: #401567;
+}
+
 .contact h1, .contact h1 {
   margin-top: 120px;
   font-size: 48pt;
@@ -241,7 +248,7 @@ h2.display-2 {
 footer {
   background-color: #222;
   padding: 40px;
-  margin-top: 50px;
+  padding-top: 50px;
   font-size: 10pt;
   text-align: center;
 }
@@ -254,15 +261,17 @@ footer:hover {
 }
 
 #explore-addresses-container {
-  padding-top: 300px;
-  padding-bottom: 300px;
-
-  background-position: bottom;
+  margin-top: 100px;
+  margin-bottom: 100px;
+  padding-top: 200px;
+  min-height: 600px;
   background-size: 100%;
   background-image: url('https://raw.githubusercontent.com/akaalias/pudding/main/public/img/explore-address.png');
 }
 
 #explore-addresses-container h2 {
   padding: 30px;
+  transform: rotate(-5deg);
+
 }
 </style>
