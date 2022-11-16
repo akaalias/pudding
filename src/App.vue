0e740cdb479bf93dc0696c9127761b58cf8f8b4a
Alexis Rondeau <alexis.rondeau@gmail.com>
2022-11-16 12:27:24 +0100

    Updates app.js

--- src/App.vue
+++ src/App.vue
@@ -13,22 +13,22 @@ document.title = "Pudding Research";
 export default Vue.extend({
   name: 'App',
   metaInfo: {
-    title: 'Pudding Research – Find better questions sooner.',
+    title: 'Pudding Research – Find Better Questions Sooner With Forensic Crypto Ecosystem Reconnaissance',
     titleTemplate: '%s',
     htmlAttrs: {
       lang: 'en-US'
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'og:title', content: 'Pudding Research – Find better questions sooner.' },
-      { name: 'og:description', content: 'Forensic Ecosystem Reconnaissance for Crypto Analysts'},
+      { name: 'og:title', content: 'Pudding Research' },
+      { name: 'og:description', content: 'Find Better Questions Sooner With Forensic Crypto Ecosystem Reconnaissance'},
       { name: 'og:url', content: 'https://getpudding.app/' },
       { name: 'og:image', content: 'https://raw.githubusercontent.com/akaalias/pudding/main/public/img/demo.png' },
       { name: 'og:type', content: 'Website' },
-      { name: 'og:site_name', content: 'Pudding Research – Find better questions sooner.'},
-      { name: 'twitter:card', content: 'Forensic Ecosystem Reconnaissance for Crypto Analysts'},
+      { name: 'og:site_name', content: 'Pudding Research'},
+      { name: 'twitter:card', content: 'Pudding Research – Find Better Questions Sooner With Forensic Crypto Ecosystem Reconnaissance'},
       { name: 'twitter:image', content: 'https://raw.githubusercontent.com/akaalias/pudding/main/public/img/demo.png'},
-      { name: 'twitter:title', content: 'Pudding Research – Find better questions sooner.'},
+      { name: 'twitter:title', content: 'Pudding - Pudding Research – Find Better Questions Sooner With Forensic Crypto Ecosystem Reconnaissance'},
       { name: 'author', content: 'Alexis Rondeau' },
     ]
   },
